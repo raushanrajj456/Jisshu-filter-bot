3a0f7fcbc930d4ca76ac5cb9a571640137fbad22
raushanrajj456 <raushanrajj456@gmail.com>
2025-08-12 21:08:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ PORT = environ.get("PORT", "8082")
 # Owners
 ADMINS = [
     int(admin) if id_pattern.search(admin) else admin
-    for admin in environ.get("ADMINS", "7612214919").split()
+    for admin in environ.get("ADMINS", "7612214919 7861690278").split()
 ]
 OWNER_USERNAME = environ.get(
     "OWNER_USERNAME", "Raushan_Rsd"
